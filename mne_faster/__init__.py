59fae9adfe531896b44dc1d8a7e549747ac93caa
Marijn van Vliet <w.m.vanvliet@gmail.com>
2022-02-26 10:10:41 +0200

    Bump version

--- mne_faster/__init__.py
+++ mne_faster/__init__.py
@@ -1,4 +1,4 @@
 from .faster import (find_bad_channels, find_bad_channels_in_epochs,
                      find_bad_components, find_bad_epochs, run_faster, hurst)
 
-__version__ = 1.0
+__version__ = 1.1
